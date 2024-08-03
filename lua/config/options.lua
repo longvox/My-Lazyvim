1752818f8d03d4b1ab8b5352cd2155d95c1f990d
Long Vo <longvox98@gmail.com>
2024-08-03 10:14:48 +0700

    feat: Show higlight color for trail space and tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,7 +16,7 @@ vim.opt.grepprg = "rg --vimgrep --smart-case --"
 vim.opt.hidden = true -- Enable modified buffers in background
 vim.opt.shortmess:append({ W = true, I = true, c = true })
 vim.opt.showmode = false -- dont show mode since we have a statusline
-vim.opt.listchars = "space:·,trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
+vim.opt.listchars = "space:·,trail:█,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
 vim.opt.pumblend = 10
 vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "120"
@@ -34,7 +34,7 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
-
+    
 -- Set encoding to utf-8
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
@@ -43,6 +43,12 @@ vim.opt.fileencoding = "utf-8"
 vim.g.autoformat = false -- globally
 vim.b.autoformat = false -- buffer-local
 
+-- disable diagnostic
+vim.diagnostic.enable(false)
+vim.cmd [[
+  syntax match TrailingSpaceTab /[\s\t]\+$/
+  hi TrailingSpaceTab guibg=gray
+]]
 -- disable some fanzy UI stuff when run in Neovide
 if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
